4af2116cb0b077cb90ec28e4f57a11962d97fe18
Javier Diaz <coderdiaz@gmail.com>
2018-03-28 13:27:19 -0600

    Fixed plugin installation

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 import TinyPagination from './src/components/TinyPagination.vue'
 
 const install = (Vue, opts ={}) => {
-  if (install.installed) return
-  install.installed = true
   Vue.component('TinyPagination', TinyPagination)
 }
 
-const plugin = {
-  version: '0.2.1-beta.6',
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+module.exports = {
+  version: '0.2.1-beta.7',
   install,
   TinyPagination
 }
 
-let GlobalVue = null
-if (typeof window !== 'undefined') {
-	GlobalVue = window.Vue
-} else if (typeof global !== 'undefined') {
-	GlobalVue = global.Vue
-}
-if (GlobalVue) {
-	GlobalVue.use(plugin)
-}
-
-module.exports = plugin
 module.exports.default = module.exports
