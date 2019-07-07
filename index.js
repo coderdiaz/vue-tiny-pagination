c740cfce4db6106ee91890fa2e1edc021b53bba4
Javier Diaz <hello@coderdiaz.me>
2019-07-07 16:49:02 -0500

    feature: updated entryfile for new approach

---
+++ index.js
@@ -0,0 +1,33 @@
+import TinyPagination from './packages/TinyPagination/index';
+
+const components = [
+  TinyPagination,
+];
+
+export function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  components.forEach((component) => {
+    Vue.component(component.name, component);
+  });
+}
+
+const plugin = {
+  install,
+};
+
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+export {
+  TinyPagination,
+};
+
+export default plugin;
